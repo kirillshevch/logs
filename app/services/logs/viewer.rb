70ba7e397c156747bb9f4c2fad498ad387adfe6a
Kirill Shevchenko <kirills167@gmail.com>
2017-11-26 21:48:47 +0200

    pagination in progress

--- app/services/logs/viewer.rb
+++ app/services/logs/viewer.rb
@@ -1,7 +1,7 @@
 module Logs
   class Viewer
     def self.call(file_name)
-      File.open('log/' + file_name + '.log').read
+      File.open('log/' + file_name + '.log').read#.readlines[0..49].join
     end
   end
 end
